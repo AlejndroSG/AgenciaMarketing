b6c2a5c3578c7b56c8f023c1809a852f25af4126
AlejndroSG <alex2005sg@gmail.com>
2024-11-15 13:04:33 +0100

    Poner los SVG del banner

--- js.js
+++ js.js
@@ -1,10 +1,38 @@
 // Variables
-const ig = document.querySelector(".ig")
-const fb = document.querySelector('.fb')
+const ig = document.querySelector("#RRSS .ig");
+const igPath = document.querySelector("#RRSS .ig path");
+const fb = document.querySelector('.fb');
+const fbPath = document.querySelector("#RRSS .fb path");
+const mail = document.querySelector(".mail svg");
+const was = document.querySelector(".was svg");
+// const 
 
 document.addEventListener('DOMContentLoaded', () => {
-    ig.addEventListener("click", () => {
-        ig.style.fill = 'yellow';
-        alert("Dentro");
+    ig.addEventListener("mouseover", () => {
+        igPath.style.fill = 'pink';
+    })
+    ig.addEventListener("mouseleave", () => {
+        igPath.style.fill = 'white';
+    })
+
+    fb.addEventListener("mouseover", () => {
+        fbPath.style.fill = 'rgba(43, 43, 247)';
+    })
+    fb.addEventListener("mouseleave", () => {
+        fbPath.style.fill = 'white';
+    })
+
+    mail.addEventListener("mouseleave", () =>{
+        mail.style.scale = 1;
+    })
+    mail.addEventListener("mouseover", () =>{
+        mail.style.scale = 1.5;
+    })
+
+    was.addEventListener("mouseover", () => {
+        was.style.scale = 1.5;
+    })
+    was.addEventListener("mouseleave", () => {
+        was.style.scale = 1;
     })
 })
